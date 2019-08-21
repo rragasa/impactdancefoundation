adee8597225e711971cce08d2fcbd41a9760e935
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-21 22:32:03 +0100

    Home page skeleton with routing

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+
+const Navigation = () => {
+  return (
+    <Box>
+      <nav role="navigation" aria-label="Main">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </nav>
+    </Box>
+  );
+}
+
+export default Navigation;

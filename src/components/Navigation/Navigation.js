aeabfb3d98e08a5f861c11d7900a03dc494788d3
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-23 17:03:39 +0100

    hide/show nav links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,22 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Box from '@material-ui/core/Box';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Navigation = () => {
-  return (
-    <Box component="div">
-      <nav role="navigation" aria-label="Main">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/who-we-are">Who We Are</Link></li>
-          <li><Link to="/our-work">Our Work</Link></li>
-          <li><Link to="/impact-youth">Impact Youth</Link></li>
-          <li><Link to="/the-team">The Team</Link></li>
-          <li><Link to="/contact-us">Contact Us</Link></li>
-        </ul>
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    display: 'flex',
+    float: 'right',
+    marginRight: theme.spacing(4),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menuIcon: {
+    fontSize: '50px',
+  }
+});
+export class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showMenu: false };
+    this.handleNavigation = this.handleNavigation.bind(this);
+  }
+
+  handleNavigation = () => {
+    this.setState(state => ({
+      showMenu: !state.showMenu
+    }));
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { showMenu } = this.state;
+
+    return (
+      <nav role="navigation" aria-label="Main" className={classes.root}>
+        <IconButton
+          onClick={this.handleNavigation}
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon className={classes.menuIcon}/>
+        </IconButton>
+        { showMenu && (
+            <ul className="nav-links">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/who-we-are">Who We Are</Link></li>
+              <li><Link to="/our-work">Our Work</Link></li>
+              <li><Link to="/impact-youth">Impact Youth</Link></li>
+              <li><Link to="/the-team">The Team</Link></li>
+              <li><Link to="/contact-us">Contact Us</Link></li>
+            </ul>
+          )
+        }
       </nav>
-    </Box>
-  );
+    );
+  }
 }
 
-export default Navigation;
+export default withStyles(styles)(Navigation);

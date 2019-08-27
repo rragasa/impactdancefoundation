b5b518e43670b8da7d5a178953d24477cb8ff4e3
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-27 12:46:19 +0100

    Adding brand font

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
-const HomeHero = () => {
-  return (
-    <Box
-      component="div"
-      className="clearfix"
-      m={4}
-    >
-      <p>Miguel Santos</p>
-    </Box>
-  );
+const styles = theme => ({
+  strapLine: {
+    fontFamily: 'league_gothicregular',
+  },
+});
+export class HomeHero extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Box
+        component="div"
+        className="clearfix"
+        m={4}
+      >
+        <Typography
+          variant="h1"
+          component="h1"
+          className={classes.strapLine}
+          gutterBottom
+        >
+          TO MOTIVATE, LEAD AND INSPIRE 'ORDINARY' PEOPLE TO ACHIEVE 'EXTRA-ORDINARY' THINGS THROUGH THE MEDIUM OF DANCE
+        </Typography>
+      </Box>
+    );
+  }
 }
 
-export default HomeHero;
+export default withStyles(styles)(HomeHero);

04218893b8d7247243594bde24ddf9d840af96f6
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-27 12:59:08 +0100

    adjusting strapline width

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   strapLine: {
     fontFamily: 'league_gothicregular',
     color: '#4B2E62',
+    '@media (max-width:600px)': {
+      fontSize: '50px',
+    },
   },
 });
 export class HomeHero extends Component {
@@ -18,14 +22,18 @@ export class HomeHero extends Component {
         className="clearfix"
         m={4}
       >
-        <Typography
-          variant="h1"
-          component="h1"
-          className={classes.strapLine}
-          gutterBottom
-        >
-          TO MOTIVATE, LEAD AND INSPIRE 'ORDINARY' PEOPLE TO ACHIEVE 'EXTRA-ORDINARY' THINGS THROUGH THE MEDIUM OF DANCE
-        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12} md={8}>
+            <Typography
+              variant="h1"
+              component="h1"
+              className={classes.strapLine}
+              gutterBottom
+            >
+              TO MOTIVATE, LEAD AND INSPIRE 'ORDINARY' PEOPLE TO ACHIEVE 'EXTRA-ORDINARY' THINGS THROUGH THE MEDIUM OF DANCE
+            </Typography>
+          </Grid>
+        </Grid>
       </Box>
     );
   }

9a6a694303736e51a108a9897f0e6abb1b9060ae
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-27 13:31:31 +0100

    adding home hero

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import HeroImg from '../../css/assets/imgs/IMPACT_YOUTH_LIVEVIBE_3-TheDukeLDN.jpg';
+import { sizing } from '@material-ui/system';
 
 const styles = theme => ({
   strapLine: {
@@ -17,24 +19,27 @@ export class HomeHero extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Box
-        component="div"
-        className="clearfix"
-        m={4}
-      >
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={8}>
-            <Typography
-              variant="h1"
-              component="h1"
-              className={classes.strapLine}
-              gutterBottom
-            >
-              TO MOTIVATE, LEAD AND INSPIRE 'ORDINARY' PEOPLE TO ACHIEVE 'EXTRA-ORDINARY' THINGS THROUGH THE MEDIUM OF DANCE
-            </Typography>
+      <Fragment>
+        <Box
+          component="div"
+          className="clearfix"
+          m={4}
+        >
+          <Grid container spacing={3} m={4}>
+            <Grid item xs={12} sm={12} md={8}>
+              <Typography
+                variant="h1"
+                component="h1"
+                className={classes.strapLine}
+                gutterBottom
+              >
+                TO MOTIVATE, LEAD AND INSPIRE 'ORDINARY' PEOPLE TO ACHIEVE 'EXTRA-ORDINARY' THINGS THROUGH THE MEDIUM OF DANCE
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+        <img src={HeroImg} width="100%" />
+      </Fragment>
     );
   }
 }

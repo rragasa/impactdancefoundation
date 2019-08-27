c2c503ae0d6fb5795437b92a2ca39b6bd45fb28e
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-27 17:17:17 +0100

    fixing underline for strapline

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -5,13 +5,17 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import HeroImg from '../../css/assets/imgs/IMPACT_YOUTH_LIVEVIBE_3-TheDukeLDN.jpg';
 import { sizing } from '@material-ui/system';
+import { display } from '@material-ui/system';
 
 const styles = theme => ({
   strapLine: {
     fontFamily: 'league_gothicregular',
+    fontSize: '96px',
+    lineHeight: '96px',
     color: '#4B2E62',
     '@media (max-width:600px)': {
-      fontSize: '50px',
+      fontSize: '36px',
+      lineHeight: '36px',
     },
   },
   heroImg: {
@@ -26,13 +30,12 @@ export class HomeHero extends Component {
     const { classes } = this.props;
     return (
       <Fragment>
-        <span className="line"></span>
         <Box
           component="div"
           className="clearfix"
           m={4}
         >
-          <Grid container spacing={3} m={4}>
+          {/* <Grid container spacing={3} m={4}>
             <Grid item xs={12} sm={9} md={8} lg={5}>
               <Typography
                 variant="h1"
@@ -40,10 +43,21 @@ export class HomeHero extends Component {
                 className={classes.strapLine}
                 gutterBottom
               >
-                TO MOTIVATE, LEAD AND INSPIRE 'ORDINARY' PEOPLE TO ACHIEVE 'EXTRA-ORDINARY' THINGS THROUGH THE MEDIUM OF DANCE
+                <span display="block">TO MOTIVATE, LEAD</span>
+                <span display="block">AND INSPIRE 'ORDINARY'</span>
+                <span display="block">PEOPLE TO ACHIEVE</span>
+                <span display="block">'EXTRA-ORDINARY'</span>
+                <span display="block">THINGS THROUGH THE</span>
+                <span display="block">MEDIUM OF DANCE</span>
               </Typography>
             </Grid>
-          </Grid>
+          </Grid> */}
+                <div className={classes.strapLine}>TO MOTIVATE, LEAD</div>
+                <div className={classes.strapLine}>AND INSPIRE 'ORDINARY'</div>
+                <div className={classes.strapLine}>PEOPLE TO ACHIEVE</div>
+                <div className="strap-underline">'EXTRA-ORDINARY'</div>
+                <div className={classes.strapLine}>THINGS THROUGH THE</div>
+                <div className={classes.strapLine}>MEDIUM OF DANCE</div>
         </Box>
         <img src={HeroImg} className={classes.heroImg} />
       </Fragment>

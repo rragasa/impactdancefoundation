9bfeffde313a1c27c5700fb8492fe4825b1cb995
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-30 19:18:30 +0100

    who we are page heading

--- src/pages/WhoWeAre/WhoWeAre.js
+++ src/pages/WhoWeAre/WhoWeAre.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import HeaderImg from '../../css/assets/imgs/GORM-8493.jpg';
 import HeroRip from '../../css/assets/imgs/hero-rip-pages.svg';
 
@@ -75,8 +76,17 @@ const styles = theme => ({
       height: '8rem',
     },
     '@media (min-width: 1280px)': {
-      height: '18rem',
+      height: '9rem',
     },
+  },
+  pageHeading: {
+    marginTop: '-44px',
+    padding: theme.spacing(2),
+  },
+  subHeading: {
+    fontFamily: 'league_gothicregular',
+    marginBottom: '15px',
+    color: '#777',
   },
 });
 export class WhoWeAre extends Component {
@@ -99,8 +109,43 @@ export class WhoWeAre extends Component {
           </div>
           <div className={classes.rip}></div>
         </Box>
-        <Box className={classes.root}>
-          <h1 className="strap-underline">Who We Are</h1>
+        <Box className={[classes.root, classes.pageHeading]}>
+          <h3 className={classes.subHeading}>ABOUT</h3>
+          <h1 className="strap-underline">WHO WE ARE</h1>
+
+          <Typography
+            variant="p"
+            component="p"
+            gutterBottom
+          >
+            For nearly 25 years, Impact Dance has motivated, inspired and nurtured
+            young people through hip-hop and street dance. Founded by Hakeem Onibudo,
+            the company has been a leader in the hip-hop, street and youth dance sector
+            since its inception in 1995.
+          </Typography>
+          <Typography
+            variant="p"
+            component="p"
+            gutterBottom
+          >
+            Through dance training, mentoring, creative performances and international
+            projects, we enable young people of all backgrounds - particularly those
+            less privileged and considered 'hard-to-reach' - to develop their creative
+            and life skills, to discover their talent and grow up to be 'fully functioning
+            individuals'; Impact alumini have gone on to thrive as, for example,
+            professional dance artists, investment bankers, radio presenters and designers.
+          </Typography>
+          <Typography
+            variant="p"
+            component="p"
+            gutterBottom
+          >
+            Based and working in Brent, with a London-wide reach and a global footprint,
+            Impact Dance is at a pivotal point in its history. Responding to a local need,
+            we are scaling up our operation to develop opportunities where there are none,
+            to invest in young people's futures, and to ensure we can continue these
+            efforts for years to come.
+          </Typography>
         </Box>
       </Box>
     );

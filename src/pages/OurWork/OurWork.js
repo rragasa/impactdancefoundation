ce7f3027f725799561d1d9b8ce477d863d7c46ef
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-09-11 18:09:23 +0100

    updating PDF on our work

--- src/pages/OurWork/OurWork.js
+++ src/pages/OurWork/OurWork.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import HeaderImg from '../../css/assets/imgs/IMG_4594.png';
-import outreachPDF from '../../css/assets/imgs/Outreach-Engagement.pdf';
+import outreachPDF from '../../css/assets/imgs/Outreach&Engagement.pdf';
 
 const styles = theme => ({
   root: {
